441dd84064067473830edee76d92b93f42916cad
augsun <codersun@126.com>
2018-06-11 23:16:59 +0800

    push by autoRelease.py

--- JXKit/Classes/Category/UICollectionView/UICollectionView+JXCategory.h
+++ JXKit/Classes/Category/UICollectionView/UICollectionView+JXCategory.h
@@ -10,9 +10,19 @@
 
 #define COLLECTIONVIEW_DEQUEUE(identifier) [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath]
 
+#define COLLECTIONVIEW_DEQUEUE_HEADER(identifier) [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:identifier forIndexPath:indexPath]
+
+#define COLLECTIONVIEW_DEQUEUE_FOOTER(identifier) [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:identifier forIndexPath:indexPath]
+
 @interface UICollectionView (JXCategory)
 
 - (void)jx_regCellNib:(Class)cellClass identifier:(NSString *)identifier;
 - (void)jx_regCellNib:(Class)cellClass identifier:(NSString *)identifier bundle:(NSBundle *)bundle;
+
+- (void)jx_regSectionHeader:(Class)headerClass identifier:(NSString *)identifier;
+- (void)jx_regSectionHeader:(Class)headerClass identifier:(NSString *)identifier bundle:(NSBundle *)bundle;
+
+- (void)jx_regSectionFooter:(Class)footerClass identifier:(NSString *)identifier;
+- (void)jx_regSectionFooter:(Class)footerClass identifier:(NSString *)identifier bundle:(NSBundle *)bundle;
 
 @end
--- JXKit/Classes/Category/UICollectionView/UICollectionView+JXCategory.m
+++ JXKit/Classes/Category/UICollectionView/UICollectionView+JXCategory.m
@@ -19,4 +19,32 @@
     [self registerNib:cellNib forCellWithReuseIdentifier:identifier];
 }
 
+- (void)jx_regSectionHeader:(Class)headerClass identifier:(NSString *)identifier {
+    [self jx_regSectionHeader:headerClass identifier:identifier bundle:nil];
+}
+
+- (void)jx_regSectionHeader:(Class)headerClass identifier:(NSString *)identifier bundle:(NSBundle *)bundle {
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass([headerClass class]) bundle:bundle];
+    [self registerNib:nib forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:identifier];
+}
+
+- (void)jx_regSectionFooter:(Class)footerClass identifier:(NSString *)identifier {
+    [self jx_regSectionFooter:footerClass identifier:identifier bundle:nil];
+}
+
+- (void)jx_regSectionFooter:(Class)footerClass identifier:(NSString *)identifier bundle:(NSBundle *)bundle {
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass([footerClass class]) bundle:bundle];
+    [self registerNib:nib forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:identifier];
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
