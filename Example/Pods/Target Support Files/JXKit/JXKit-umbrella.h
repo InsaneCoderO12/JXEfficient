315421982c1addab827aacea80db0a1d609dd9ac
augsun <codersun@126.com>
2018-09-05 09:29:36 +0800

    push by autoRelease.py

--- Example/Pods/Target Support Files/JXKit/JXKit-umbrella.h
+++ Example/Pods/Target Support Files/JXKit/JXKit-umbrella.h
@@ -10,7 +10,24 @@
 #endif
 #endif
 
+#import "NSDate+JXCategory.h"
+#import "NSString+JXCategory.h"
+#import "UIButton+JXCategory.h"
+#import "UICollectionView+JXCategory.h"
+#import "UIImage+JXCategory.h"
+#import "UIScrollView+JXCategory.h"
+#import "UITableView+JXCategory.h"
+#import "UIView+JXCategory.h"
+#import "UIView+JXToastAndProgressHUD.h"
+#import "JXInline.h"
+#import "JXJSONCache.h"
 #import "JXKit.h"
+#import "JXLocationCoordinates.h"
+#import "JXMacro.h"
+#import "JXRegular.h"
+#import "JXSystemAlert.h"
+#import "JXUpdateCheck.h"
+#import "JXUUIDAndKeyChain.h"
 
 FOUNDATION_EXPORT double JXKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char JXKitVersionString[];
