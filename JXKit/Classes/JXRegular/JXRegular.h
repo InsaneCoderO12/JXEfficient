a3935a60467e46d345dcc1692f592a086c84fdba
augsun <codersun@126.com>
2018-09-19 18:44:40 +0800

    push by autoRelease.py

--- JXKit/Classes/JXRegular/JXRegular.h
+++ JXKit/Classes/JXRegular/JXRegular.h
@@ -7,52 +7,52 @@
 @interface JXRegular : NSObject
 
 // 验证 Pwd 满足 6-20位 的a-z A-Z 0-9组成
-+ (BOOL)regularPwdLength_6to20_with_aAtozZ_or_0to9:(NSString *)pwd;
++ (BOOL)jx_regularPwdLength_6to20_with_aAtozZ_or_0to9:(NSString *)pwd;
 
 // 验证 Email
-+ (BOOL)regularEmail:(NSString *)email;
++ (BOOL)jx_regularEmail:(NSString *)email;
 
 // 验证 Url
-+ (BOOL)regularUrl:(NSString *)url;
++ (BOOL)jx_regularUrl:(NSString *)url;
 
 // 验证正数
-+ (BOOL)regularPositiveNumber:(NSString *)number;
++ (BOOL)jx_regularPositiveNumber:(NSString *)number;
 
 // 非负浮点数
-+ (BOOL)regularUnNegativeFloat:(NSString *)unNegativeFloat;
++ (BOOL)jx_regularUnNegativeFloat:(NSString *)unNegativeFloat;
 
 // 验证用户名 满足 6-20位 的a-z A-Z 0-9组成
-+ (BOOL)regularUserNameLength_3to20_with_aAtozZ_or_0to9:(NSString *)userName;
++ (BOOL)jx_regularUserNameLength_3to20_with_aAtozZ_or_0to9:(NSString *)userName;
 
 // 验证手机号
-+ (BOOL)regularMobile:(NSString *)mobile;
++ (BOOL)jx_regularMobile:(NSString *)mobile;
 
 // 验证6位数字验证码
-+ (BOOL)regularAuthCode6:(NSString *)code6;
++ (BOOL)jx_regularAuthCode6:(NSString *)code6;
 
 // 验证昵称 满足中文 大小写字母 数字
-+ (BOOL)regularNickName:(NSString *)nickName;
++ (BOOL)jx_regularNickName:(NSString *)nickName;
 
 // 验证中文
-+ (BOOL)regularChinese:(NSString *)chinese;
++ (BOOL)jx_regularChinese:(NSString *)chinese;
 
 // 验证是否有空格
-+ (BOOL)regularHaveSpace:(NSString *)string;
++ (BOOL)jx_regularHaveSpace:(NSString *)string;
 
 // 验证单个字符是否是 ASCII 码
-+ (BOOL)regularIsAscii:(NSString *)ascii;
++ (BOOL)jx_regularIsAscii:(NSString *)ascii;
 
 // 验证是否 数字 大小写字母
-+ (BOOL)regularNumericAndLetter:(NSString *)string;
++ (BOOL)jx_regularNumericAndLetter:(NSString *)string;
 
 // 验证字母
-+ (BOOL)regularLetter:(NSString *)string;
++ (BOOL)jx_regularLetter:(NSString *)string;
 
 // 昵称(满足中文 大小写字母 数字)的长度 (中文当两个字符, 数字 字母为一个字符)
-+ (NSInteger)regularLengthOfNickName:(NSString *)nickName;
++ (NSInteger)jx_regularLengthOfNickName:(NSString *)nickName;
 
 // 验证 18 位身份证
-+ (BOOL)regularIDCard:(NSString *)idCard;
++ (BOOL)jx_regularIDCard:(NSString *)idCard;
 
 @end
 
