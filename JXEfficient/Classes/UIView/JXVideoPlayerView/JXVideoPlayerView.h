de208b8a3bdecd70019daecda4dc46fb3c659425
augsun <codersun@126.com>
2019-01-23 00:35:47 +0800

    push by autoRelease_JXEfficient.py

--- JXEfficient/Classes/UIView/JXVideoPlayerView/JXVideoPlayerView.h
+++ JXEfficient/Classes/UIView/JXVideoPlayerView/JXVideoPlayerView.h
@@ -11,34 +11,48 @@
 NS_ASSUME_NONNULL_BEGIN
 
 typedef NS_ENUM(NSUInteger, JXVideoPlayerViewStatus) {
-    JXVideoPlayerViewStatusUnavailable = 0,     // 不可用
-    JXVideoPlayerViewStatusReadyToPlay,         // 将要播放
-    JXVideoPlayerViewStatusPlaying,             // 播放中
-    JXVideoPlayerViewStatusPause,               // 暂停中
-    JXVideoPlayerViewStatusEndPlaying,          // 结束播放
-    JXVideoPlayerViewStatusFailure,             // 播放失败
+    JXVideoPlayerViewStatusUnavailable = 0,         // 不可用
+    JXVideoPlayerViewStatusCannotBePlayedOrUnknown, // 不能播放
+    
+    JXVideoPlayerViewStatusDidSetURL,               // 已设置 URL
+    
+    JXVideoPlayerViewStatusPlaying,                 // 播放中
+    JXVideoPlayerViewStatusPause,                   // 暂停中
+    JXVideoPlayerViewStatusEndPlaying,              // 结束播放
+    
+    JXVideoPlayerViewStatusFailure,                 // 播放失败
 };
 
 @interface JXVideoPlayerView : UIView
 
 + (instancetype)videoPlayerView; // 指定初始化器
 
-@property (nonatomic, readonly) UIProgressView *progressView;
++ (void)firstVideoFrameForURL:(NSURL *)URL completion:(void (^)(UIImage * _Nullable img))completion; // 获取第一帧图片
 
-@property (nonatomic, strong) NSURL *URL;
-+ (void)firstVideoFrameForURL:(NSURL *)URL completion:(void (^)(UIImage * _Nullable img))completion;
+@property (nonatomic, readonly) UIProgressView *progressView; // 播放进度 不显示可以 hidden 掉, 默认显示.
+
+/*
+ cell 复用的情况 showFirstVideoFrame 传入 NO 以节约性能
+ 同时 调用 firstVideoFrameForURL: completion: 方法异步加载第一帧图片 自行在 JXVideoPlayerView 创建一个 UIImageView 控制预览及视频播放层的隐藏状态.
+ firstVideoFrameForURL: completion: 方法没有缓存 注意 cell 复用情况的性能花销
+ */
+- (void)setURL:(NSURL *)URL showFirstVideoFrame:(BOOL)showFirstVideoFrame;
 
 @property (nonatomic, readonly) JXVideoPlayerViewStatus status;
+@property (nonatomic, copy, nullable) void (^statusDidChanged)(JXVideoPlayerViewStatus status); // 状态改变回调
 
-@property (nonatomic, copy) void (^cannotBePlayedOrUnknown)(void); // 不能播放 <还未播放>
-@property (nonatomic, copy) void (^failedToPlay)(void); // 播放失败 <已经播放>
-@property (nonatomic, copy) void (^loadingProgress)(CGFloat loadedTime, CGFloat duration); // 缓冲进度
-@property (nonatomic, copy) void (^readyToPlay)(CGFloat duration); // 播放就绪
-@property (nonatomic, copy) void (^playingProgress)(CGFloat currentTime, CGFloat duration); // 播放进度
-@property (nonatomic, copy) void (^didFinishedPlay)(void); // 结束播放
+@property (nonatomic, readonly) CGFloat duration; // canPlay == YES 时才有值
 
-- (BOOL)play;
-- (BOOL)rePlay;
+@property (nonatomic, copy, nullable) void (^loadingProgress)(CGFloat loadedTime, CGFloat duration); // 调用 play 方法之后的 缓冲进度
+@property (nonatomic, copy, nullable) void (^playingProgress)(CGFloat currentTime, CGFloat duration); // 调用 play 方法之后的 播放进度
+
+@property (nonatomic, readonly) BOOL canPlay;
+- (void)play;
+
+@property (nonatomic, readonly) BOOL canReplay;
+- (void)replay;
+
+@property (nonatomic, readonly) BOOL canPause;
 - (void)pause;
 
 @end
--- JXEfficient/Classes/UIView/JXVideoPlayerView/JXVideoPlayerView.m
+++ JXEfficient/Classes/UIView/JXVideoPlayerView/JXVideoPlayerView.m
@@ -29,6 +29,10 @@ static const CGFloat kProgressViewHeight = 3.0;
     return (AVPlayerLayer *)self.layer;
 }
 
+- (void)dealloc {
+//    NSLog(@"dealloc -> JXAVPlayerLayerView");
+}
+
 @end
 
 // ====================================================================================================
@@ -37,9 +41,17 @@ static const CGFloat kProgressViewHeight = 3.0;
 @property (nonatomic, strong) AVPlayerItem *avPlayerItem;
 @property (nonatomic, strong) AVPlayer *player;
 
-@property (nonatomic, assign) CGFloat duration;
-
 @property (nonatomic, strong) JXAVPlayerLayerView *playerLayerView;
+@property (nonatomic, strong) UIImageView *previewImgView;
+
+@property (nonatomic, strong) NSURL *URL;
+@property (nonatomic, copy) NSURL *URL_previous;
+
+@property (nonatomic, assign) JXVideoPlayerViewStatus realStatus;
+
+@property (nonatomic, assign) BOOL videoPlayerReadyToPlay;
+
+@property (nonatomic, strong) NSMutableDictionary <NSString *, id> *observers;
 
 @end
 
@@ -50,29 +62,29 @@ static const CGFloat kProgressViewHeight = 3.0;
     return view;
 }
 
++ (void)firstVideoFrameForURL:(NSURL *)URL completion:(void (^)(UIImage * _Nullable))completion {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:URL options:nil];
+        AVAssetImageGenerator *assetGen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+        assetGen.appliesPreferredTrackTransform = YES;
+        CMTime time = CMTimeMakeWithSeconds(0.0, 60);
+        CGImageRef imgRef = [assetGen copyCGImageAtTime:time actualTime:NULL error:NULL];
+        UIImage *image = [[UIImage alloc] initWithCGImage:imgRef];
+        CGImageRelease(imgRef);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            JX_BLOCK_EXEC(completion, image);
+        });
+    });
+}
+
 - (instancetype)init {
     self = [super init];
     if (self) {
         self.backgroundColor = [UIColor blackColor];
-        _status = JXVideoPlayerViewStatusUnavailable;
-        
-        // 播放完成
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(avPlayerItemDidPlayToEndTimeNotification:)
-                                                     name:AVPlayerItemDidPlayToEndTimeNotification
-                                                   object:nil];
-        // 播放失败
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(avPlayerItemFailedToPlayToEndTimeNotification:)
-                                                     name:AVPlayerItemFailedToPlayToEndTimeNotification
-                                                   object:nil];
-        // 异常中断
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(avPlayerItemPlaybackStalledNotification:)
-                                                     name:AVPlayerItemPlaybackStalledNotification
-                                                   object:nil];
-        
-        //
+        self.realStatus = JXVideoPlayerViewStatusUnavailable;
+        self.observers = [[NSMutableDictionary alloc] init];
+
+        // playerLayerView
         self.playerLayerView = [[JXAVPlayerLayerView alloc] init];
         [self addSubview:self.playerLayerView];
         self.playerLayerView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -98,7 +110,32 @@ static const CGFloat kProgressViewHeight = 3.0;
         
         self.playerLayerView.playerLayer.videoGravity = AVLayerVideoGravityResize;
         
-        //
+        // previewImgView
+        self.previewImgView = [[UIImageView alloc] init];
+        [self addSubview:self.previewImgView];
+        self.previewImgView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.previewImgView.backgroundColor = [UIColor blackColor];
+        [self addConstraints:@[
+                               [NSLayoutConstraint constraintWithItem:self.previewImgView attribute:NSLayoutAttributeLeft
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self attribute:NSLayoutAttributeLeft
+                                                           multiplier:1.0 constant:0.0],
+                               [NSLayoutConstraint constraintWithItem:self.previewImgView attribute:NSLayoutAttributeRight
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self attribute:NSLayoutAttributeRight
+                                                           multiplier:1.0 constant:0.0],
+                               [NSLayoutConstraint constraintWithItem:self.previewImgView attribute:NSLayoutAttributeTop
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self attribute:NSLayoutAttributeTop
+                                                           multiplier:1.0 constant:0.0],
+                               [NSLayoutConstraint constraintWithItem:self.previewImgView attribute:NSLayoutAttributeBottom
+                                                            relatedBy:NSLayoutRelationEqual
+                                                               toItem:self attribute:NSLayoutAttributeBottom
+                                                           multiplier:1.0 constant:0.0],
+                               ]
+         ];
+
+        // progressView
         _progressView = [[UIProgressView alloc] init];
         [self addSubview:self.progressView];
         self.progressView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -125,20 +162,70 @@ static const CGFloat kProgressViewHeight = 3.0;
         self.progressView.backgroundColor = [UIColor clearColor];
         self.progressView.trackTintColor = [[UIColor lightGrayColor] colorWithAlphaComponent:.6f];
 
+        // 播放完成
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(avPlayerItemDidPlayToEndTimeNotification:)
+                                                     name:AVPlayerItemDidPlayToEndTimeNotification
+                                                   object:nil];
+        // 播放失败
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(avPlayerItemFailedToPlayToEndTimeNotification:)
+                                                     name:AVPlayerItemFailedToPlayToEndTimeNotification
+                                                   object:nil];
+        // 异常中断
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(avPlayerItemPlaybackStalledNotification:)
+                                                     name:AVPlayerItemPlaybackStalledNotification
+                                                   object:nil];
         
+        // player
+        AVPlayer *player = [AVPlayer playerWithPlayerItem:nil];
+        JX_WEAK_SELF;
+        [player addPeriodicTimeObserverForInterval:CMTimeMake(1.0, 1.0) queue:nil usingBlock:^(CMTime time) {
+            JX_STRONG_SELF;
+            CGFloat currentTime = CMTimeGetSeconds(time);
+            self.progressView.progress = currentTime / self.duration;
+            self.progressView.hidden = NO;
+            self.previewImgView.hidden = YES;
+            JX_BLOCK_EXEC(self.playingProgress, currentTime, self.duration);
+        }];
+        self.player = player;
+        self.playerLayerView.playerLayer.player = self.player;
+        [self obj:self.player addObserverSelfForKey:@"rate"];
+
+
     }
     return self;
 }
 
-- (void)addSubview:(UIView *)view {
-    [super addSubview:view];
-    
+- (void)setRealStatus:(JXVideoPlayerViewStatus)realStatus {
+    if (_realStatus == realStatus) {
+        return;
+    }
+    _realStatus = realStatus;
+    _status = realStatus;
+    JX_BLOCK_EXEC(self.statusDidChanged, realStatus);
 }
 
 - (void)dealloc {
+//    NSLog(@"dealloc -> JXVideoPlayerView");
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-    [self.avPlayerItem removeObserver:self forKeyPath:@"status"];
-    [self.avPlayerItem removeObserver:self forKeyPath:@"loadedTimeRanges"];
+    for (NSString *keyEnum in self.observers.allKeys) {
+        [self removeObserverForKey:keyEnum];
+    }
+}
+
+- (void)removeObserverForKey:(NSString *)key {
+    id obj = [self.observers objectForKey:key];
+    if (obj) {
+        [obj removeObserver:self forKeyPath:key];
+    }
+}
+
+- (void)obj:(id)obj addObserverSelfForKey:(NSString *)key {
+    [self removeObserverForKey:key];
+    [self.observers setObject:obj forKey:key];
+    [obj addObserver:self forKeyPath:key options:NSKeyValueObservingOptionNew context:nil];
 }
 
 // 播放完成
@@ -146,8 +233,7 @@ static const CGFloat kProgressViewHeight = 3.0;
     id object = noti.object;
     if (object == self.avPlayerItem) {
         self.progressView.hidden = YES;
-        _status = JXVideoPlayerViewStatusEndPlaying;
-        JX_BLOCK_EXEC(self.didFinishedPlay);
+        self.realStatus = JXVideoPlayerViewStatusEndPlaying;
     }
 }
 
@@ -155,8 +241,7 @@ static const CGFloat kProgressViewHeight = 3.0;
 - (void)avPlayerItemFailedToPlayToEndTimeNotification:(NSNotification *)noti {
     id object = noti.object;
     if (object == self.avPlayerItem) {
-        _status = JXVideoPlayerViewStatusFailure;
-        JX_BLOCK_EXEC(self.failedToPlay);
+        self.realStatus = JXVideoPlayerViewStatusFailure;
     }
 }
 
@@ -164,91 +249,100 @@ static const CGFloat kProgressViewHeight = 3.0;
 - (void)avPlayerItemPlaybackStalledNotification:(NSNotification *)noti {
     id object = noti.object;
     if (object == self.avPlayerItem) {
-        _status = JXVideoPlayerViewStatusFailure;
-        JX_BLOCK_EXEC(self.failedToPlay);
+        self.realStatus = JXVideoPlayerViewStatusFailure;
     }
 }
 
-+ (void)firstVideoFrameForURL:(NSURL *)URL completion:(void (^)(UIImage * _Nullable))completion {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:URL options:nil];
-        AVAssetImageGenerator *assetGen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
-        assetGen.appliesPreferredTrackTransform = YES;
-        CMTime time = CMTimeMakeWithSeconds(0.0, 60);
-        CGImageRef imgRef = [assetGen copyCGImageAtTime:time actualTime:NULL error:NULL];
-        UIImage *image = [[UIImage alloc] initWithCGImage:imgRef];
-        CGImageRelease(imgRef);
-        dispatch_async(dispatch_get_main_queue(), ^{
-            JX_BLOCK_EXEC(completion, image);
-        });
-    });
-}
-
-- (void)setURL:(NSURL *)URL {
-
+- (void)setURL:(NSURL *)URL showFirstVideoFrame:(BOOL)showFirstVideoFrame {
     if (!URL) {
         return;
     }
+    _URL = URL;
     
-    //
-    AVPlayerItem *item = [AVPlayerItem playerItemWithURL:URL];
-    [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
-    [item addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
-    self.avPlayerItem = item;
-
-    //
-    AVPlayer *player = [AVPlayer playerWithPlayerItem:self.avPlayerItem];
-    JX_WEAK_SELF;
-    [player addPeriodicTimeObserverForInterval:CMTimeMake(1.0, 1.0) queue:nil usingBlock:^(CMTime time) {
-        JX_STRONG_SELF;
-        CGFloat currentTime = CMTimeGetSeconds(time);
-        self.progressView.progress = currentTime / self.duration;
-        self.progressView.hidden = NO;
-        JX_BLOCK_EXEC(self.playingProgress, currentTime, self.duration);
+    self.realStatus = JXVideoPlayerViewStatusDidSetURL;
+    
+    if (!showFirstVideoFrame) {
+        return;
+    }
+    
+    NSURL *tempURL = [URL copy];
+    [JXVideoPlayerView firstVideoFrameForURL:URL completion:^(UIImage * _Nullable img) {
+        if ([self.URL.absoluteString isEqualToString:tempURL.absoluteString] && img != nil) {
+            self.previewImgView.image = img;
+        }
     }];
-    self.player = player;
-
-    //
-    self.playerLayerView.playerLayer.player = self.player;
 }
 
-- (BOOL)play {
-    if (_status == JXVideoPlayerViewStatusReadyToPlay ||
-        _status == JXVideoPlayerViewStatusPause ||
-        _status == JXVideoPlayerViewStatusFailure) {
+- (BOOL)canPlay {
+    return
+    self.realStatus == JXVideoPlayerViewStatusDidSetURL ||
+    self.realStatus == JXVideoPlayerViewStatusPause ||
+    self.realStatus == JXVideoPlayerViewStatusEndPlaying ||
+    self.realStatus == JXVideoPlayerViewStatusFailure;
+}
+
+- (void)play {
+    if (self.realStatus == JXVideoPlayerViewStatusDidSetURL) {
+        [self checkIfNeedReInitial];
         
-        _status = JXVideoPlayerViewStatusPlaying;
         [self.player play];
-        return YES;
     }
-    else if (_status == JXVideoPlayerViewStatusEndPlaying) {
-        BOOL ret = [self rePlay];
-        return ret;
+    else if (self.realStatus == JXVideoPlayerViewStatusPause ||
+             self.realStatus == JXVideoPlayerViewStatusFailure) {
+        
+        [self.player play];
+    }
+    else if (self.realStatus == JXVideoPlayerViewStatusEndPlaying) {
+        [self replay];
     }
     else {
-        return NO;
+        
     }
 }
 
-- (BOOL)rePlay {
-    if (_status == JXVideoPlayerViewStatusReadyToPlay ||
-        _status == JXVideoPlayerViewStatusPlaying ||
-        _status == JXVideoPlayerViewStatusPause ||
-        _status == JXVideoPlayerViewStatusEndPlaying ||
-        _status == JXVideoPlayerViewStatusFailure ) {
+- (void)checkIfNeedReInitial {
+    if (![self.URL.absoluteString isEqualToString:self.URL_previous.absoluteString]) {
+        self.URL_previous = self.URL;
         
-        _status = JXVideoPlayerViewStatusPlaying;
+        //
+        AVPlayerItem *item = [AVPlayerItem playerItemWithURL:self.URL];
+        [self obj:item addObserverSelfForKey:@"status"];
+        [self obj:item addObserverSelfForKey:@"loadedTimeRanges"];
+        self.avPlayerItem = item;
+        
+        [self.player replaceCurrentItemWithPlayerItem:item];
+    }
+}
+
+- (BOOL)canReplay {
+    return
+    self.realStatus == JXVideoPlayerViewStatusPlaying ||
+    self.realStatus == JXVideoPlayerViewStatusPause ||
+    self.realStatus == JXVideoPlayerViewStatusEndPlaying ||
+    self.realStatus == JXVideoPlayerViewStatusFailure;
+}
+
+- (void)replay {
+    if (self.realStatus == JXVideoPlayerViewStatusPlaying ||
+        self.realStatus == JXVideoPlayerViewStatusPause ||
+        self.realStatus == JXVideoPlayerViewStatusEndPlaying ||
+        self.realStatus == JXVideoPlayerViewStatusFailure ) {
+        
         [self.player seekToTime:kCMTimeZero];
         [self.player play];
-        return YES;
     }
     else {
-        return NO;
+
     }
+}
+
+- (BOOL)canPause {
+    return
+    self.realStatus == JXVideoPlayerViewStatusPlaying;
 }
 
 - (void)pause {
-    _status = JXVideoPlayerViewStatusPause;
+    self.realStatus = JXVideoPlayerViewStatusPause;
     [self.player pause];
 }
 
@@ -257,42 +351,54 @@ static const CGFloat kProgressViewHeight = 3.0;
                         change:(NSDictionary<NSKeyValueChangeKey,id> *)change
                        context:(void *)context
 {
-    if (object != self.avPlayerItem) {
-        return;
-    }
-    if ([keyPath isEqualToString:@"status"]) {
-        NSLog(@"status");
-        AVPlayerItemStatus status = jx_intValue(change[NSKeyValueChangeNewKey]);
-        switch (status) {
-            case AVPlayerItemStatusUnknown:
-            {
-                JX_BLOCK_EXEC(self.cannotBePlayedOrUnknown);
-            } break;
-                
-            case AVPlayerItemStatusReadyToPlay:
-            {
-                self.duration = CMTimeGetSeconds(self.avPlayerItem.duration);
-                _status = JXVideoPlayerViewStatusReadyToPlay;
-                JX_BLOCK_EXEC(self.readyToPlay, self.duration);
-            } break;
-                
-            case AVPlayerItemStatusFailed:
-            {
-                JX_BLOCK_EXEC(self.cannotBePlayedOrUnknown);
-            } break;
-                
-            default: break;
+    if (object == self.avPlayerItem) {
+        if ([keyPath isEqualToString:@"status"]) {
+            AVPlayerItemStatus status = jx_intValue(change[NSKeyValueChangeNewKey]);
+            switch (status) {
+                case AVPlayerItemStatusUnknown:
+                {
+                    self.realStatus = JXVideoPlayerViewStatusCannotBePlayedOrUnknown;
+                } break;
+                    
+                case AVPlayerItemStatusReadyToPlay:
+                {
+                    _duration = CMTimeGetSeconds(self.avPlayerItem.duration);
+                    self.videoPlayerReadyToPlay = YES;
+                } break;
+                    
+                case AVPlayerItemStatusFailed:
+                {
+                    self.realStatus = JXVideoPlayerViewStatusCannotBePlayedOrUnknown;
+                } break;
+                    
+                default: break;
+            }
+        }
+        else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+            _duration = CMTimeGetSeconds(self.avPlayerItem.duration);
+            NSArray *array= self.avPlayerItem.loadedTimeRanges;
+            CMTimeRange timeRange = [array.firstObject CMTimeRangeValue];
+            CGFloat start = CMTimeGetSeconds(timeRange.start);
+            CGFloat duration = CMTimeGetSeconds(timeRange.duration);
+            CGFloat loadedTime = start + duration;
+            JX_BLOCK_EXEC(self.loadingProgress, loadedTime, self.duration);
         }
     }
-    else if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
-        NSLog(@"loadedTimeRanges");
-        self.duration = CMTimeGetSeconds(self.avPlayerItem.duration);
-        NSArray *array= self.avPlayerItem.loadedTimeRanges;
-        CMTimeRange timeRange = [array.firstObject CMTimeRangeValue];
-        CGFloat start = CMTimeGetSeconds(timeRange.start);
-        CGFloat duration = CMTimeGetSeconds(timeRange.duration);
-        CGFloat loadedTime = start + duration;
-        JX_BLOCK_EXEC(self.loadingProgress, loadedTime, self.duration);
+    else if (object == self.player) {
+        if ([keyPath isEqualToString:@"rate"]) {
+            // 播放
+            if (self.player.rate == 1.0) {
+                self.realStatus = JXVideoPlayerViewStatusPlaying;
+            }
+            // 暂停 或 结束播放
+            else if (self.player.rate == 0.0) {
+                CGFloat currentTime = CMTimeGetSeconds(self.player.currentTime);
+                CGFloat duration = self.duration;
+                if (currentTime != duration) {
+                    self.realStatus = JXVideoPlayerViewStatusPause;
+                }
+            }
+        }
     }
 }
 
